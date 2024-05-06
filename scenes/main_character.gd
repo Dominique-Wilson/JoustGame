c982322eb9a12199938717698f7bc09648f0f301
DominiqueW <git@dominiquewilson.xyz>
2024-05-05 23:38:32 -0500

    modifying main char mvmt

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@onready var sprite_2d = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -26,3 +27,6 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+var isLeft = velocity.x < 0
+Sprite2D.flip_h = isLeft
